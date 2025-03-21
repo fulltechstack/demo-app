46a6af66f5f2d1278f0557ea6e9798bffc83fa4a
dubey14 <vidhidubey14@gmail.com>
2025-03-21 17:08:25 -0400

    initial

---
+++ src/components/authentication/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <div>
+        <input type="text">Username</input>
+        <input type="password">Password</input>
+        <input type="button">Login</input>
+      </div>
+    </div>
+  );
+}
+export default Login;
